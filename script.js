dddd26f2ac3bfa76189b7dee3f45778d2ccd4b4b
“alexshroeder” <dev.alexshroeder@gmail.com>
2023-05-31 10:35:43 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleMode() {
+    const html = document.documentElement // Representação do documento em formato de objeto javascript.
+    html.classList.toggle('light') // Trocando as classes.
+}
